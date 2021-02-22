1171f7e11794ecd255d245f1e7e621130c90a186
Leevan48 <98naidooleevan@gmail.com>
2021-02-22 11:10:11 +0100

    fitst commit

---
+++ Games Sales/GameSales.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+df = pd.read_csv('dato.csv')
+
+print("No. of games per publisher :")
+print(df['Publisher'].value_counts())
+
+print("Game with maximum sales in Europe: ")
+
+
+maximum_eu = df['EU_Sales'].max()
+
+
+grouped1_df = df.groupby('EU_Sales')['Platform']
+
+for key, item in grouped1_df:
+
+    if key == maximum_eu:
+        print(grouped1_df.get_group(key))
+
+
+
+print("Game with maximum sales in North America: ")
+
+maximum_na = df['NA_Sales'].max()
+
+grouped2_df = df.groupby('NA_Sales')['Platform']
+
+for key, item in grouped2_df:
+
+    if key == maximum_na:
+        print(grouped2_df.get_group(key))
+
+
+
+
+
