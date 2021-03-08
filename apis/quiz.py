40dd4b147d463f93bec1ddaaffe9fed6eea20690
Leevan48 <98naidooleevan@gmail.com>
2021-03-08 13:09:46 +0100

    fourth commit

--- apis/quiz.py
+++ apis/quiz.py
@@ -6,27 +6,26 @@ response = requests.get('https://opentdb.com/api.php?amount=5&category=12&diffic
 
 class Quiz:
     def __init__(self):
-        self.word = ''
+        self.correct_answers = 0
 
 
     def get_question(self):
         if response.status_code != 200:
             print('Error', response.status_code)
         else:
-            data = response.json()
+            self.data = response.json()
             correct_answers = 0
             for x in range(5):
-                self.question = data['results'][x]['question']
-                self.options = data['results'][x]['incorrect_answers']
-                self.options.append(data['results'][x]['correct_answer'])
+                self.question = self.data['results'][x]['question']
+                self.options = self.data['results'][x]['incorrect_answers']
+                self.options.append(self.data['results'][x]['correct_answer'])
                 random.shuffle(self.options)
                 print(self.question)
-                answer = self.get_answer()
+                self.answer = self.get_answer()
+                self.check_answer(x)
 
-                if self.answer == data['results'][x]['correct_answer']:
-                    correct_answers += 1
+            self.display_score()
 
-            print('You got ' + str(correct_answers) + ' questions correct')
 
     def get_answer(self):
         for item in self.options:
@@ -34,6 +33,13 @@ class Quiz:
         self.answer = input()
         return self.answer
 
+    def check_answer(self,x):
+        if self.answer == self.data['results'][x]['correct_answer']:
+            self.correct_answers += 1
+            print('Correct!')
+
+    def display_score(self):
+        print('You got ' + str(self.correct_answers) + ' questions correct')
 
 
 if __name__ == '__main__':
